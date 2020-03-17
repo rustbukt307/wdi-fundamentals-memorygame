7155a2079c0304c3ad1546dc412394ec271d37bd
Tom Moliterno <rustbukt307@Toms-MacBook-Air.local>
2020-03-17 18:47:46 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,41 +1,56 @@
-const cards = [
+var cards = [
 {
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
-},
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
 {
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
-},
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
 {
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
-},
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
 {
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
-},
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	},
 ];
-const cardsInPlay = [];
+
+var cardsInPlay = [];
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	this.src = cards[cardId].cardImage;
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+};
 
 function checkForMatch() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}}
+	if (cardsInPlay.length == 2){	
+		if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+		} else {
+		alert("Sorry, try again.");
+		}
+	}
+};
 
-function flipCard(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-}
-
-flipCard(0);
-flipCard(2);
-checkForMatch();
+createBoard();
